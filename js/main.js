f2077231ab1eeb6b04da494fb49b091570d1ab87
Stefan Sokic <stefansokic@hotmail.com>
2016-05-28 20:26:04 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-var COL_1_CLICKS = 1;
+var CLICKS = 1;
 
 if ($(window).width() < 797) {
 	$(".quotes h2").css("margin-left", ("0%"));
+	$(".quotes").css("margin-top", "-50%");
 	$(".face img").css("width", "150");
 	$(".face img").css("height", "228");
 	$(".face img").css("margin-left", "-75px");
 	$(".face img").css("margin-top", "-114px");
+	$(".info").css("top", "80vh");
 	};
 
 $(document).ready(function() {
 	$(".face").click( function() {
 		for(i = 1; i <= 12; i++) {
-			if (COL_1_CLICKS == i) {
+			if (CLICKS == i) {
 				$(".quotes h2:nth-child(" + i + ")").fadeIn(10);
 			}
 		}
-		COL_1_CLICKS++;
+		CLICKS++;
 		});
 	//top text floats in and out
 	$("h4").css("transform", "translate(1650px, 0px)");
