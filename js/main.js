6df5ea266e42ff7acabb4634447f00a419adcf63
Stefan Sokic <stefansokic@hotmail.com>
2016-05-28 19:51:47 -0700

    Fix image for mobile

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 var COL_1_CLICKS = 1;
+
 if ($(window).width() < 797) {
 	$(".quotes h2").css("margin-left", ("0%"));
+	$(".face img").css("width", "150");
+	$(".face img").css("height", "228");
+	$(".face img").css("margin-left", "-75px");
+	$(".face img").css("margin-top", "-114px");
 	};
+
 $(document).ready(function() {
 	$(".face").click( function() {
-		//TODO make the "12" below a length value
 		for(i = 1; i <= 12; i++) {
 			if (COL_1_CLICKS == i) {
 				$(".quotes h2:nth-child(" + i + ")").fadeIn(10);
 			}
 		}
-//		else if (clicks == 1) {
-//			$(".quotes .column1 h2:nth-child(" + 2 + ")").fadeIn(10);
-//		}
-//		else if (clicks == 2) {
-//			$(".quotes .column1 h2:nth-child(" + 3 + ")").fadeIn(10);
-//		}
 		COL_1_CLICKS++;
 		});
 	//top text floats in and out
