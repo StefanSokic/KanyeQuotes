ddc866726275781874e495b153310ed38817f2b9
Stefan Sokic <stefansokic@hotmail.com>
2016-05-28 19:50:40 -0700

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+var QUOTES = [
+  "YOU AINT GOT THE ANSWERS SWAY",
+  "BEYONCE HAD THE BEST MUSIC VIDEO OF ALL TIME",
+  "YEEZY YEEZY YEEZY JUST JUMPED OVER JUMPMAN",
+  "HOW, SWAY??",
+  "STEVE JOBS IS DEAD",
+  "WALT DISNEY IS DEAD",
+  "George Bush Doesn't care about black people",
+  "I still think i am the greatest",
+  "I'm not a big fan of books. I am a proud non-reader of books.",
+  "I'm a creative genius and there's no other way to word it",
+  "My greatest pain in life is that I will never be able to see myself perform live",
+  "When you're the absolute best, you get hated on the most",
+  "I don't even listen to rap. My apartment is too nice to listen to rap in",
+  "I am so credible and so influential and so relevant that I will change things",
+  "Fur pillows are hard to actually sleep on",
+  "For me, first of all, dopeness is what I like the most. Dopeness. People who want to make things as dope as possible, and, by default, make money from it",
+  "I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus",
+  "I am the number one human being in music. That means any person that's living or breathing is number two",
+  "The Bible had 20, 30, 40, 50 characters in it. You don't think that I would be one of the characters of today's modern Bible???",
+  "I'm the closest that hip-hop is getting to God. In some situations I'm like a ghetto Pope",
+  "I walk through the hotel and I walk down the street and people look at me like I'm f**king insane, like I'm Hitler. One day the light will shine through, and one day people will understand everything I ever did",
+  "Mark Zuckerburg invest 1 billion dollars into Kanye West ideas",
+  "You may be talented, but you're not kanye west",
+  "sometimes i push the \"door close\" button on people running to the elevator",
+  "do you know where to find marble conferance tables? I'm looking to have a conferance... not until i get the table though",
+  "I think I deserve more",
+  "If you're a Kanye West fan, you're not a fan of me. You're a fan of yourself",
+];
+
+document.getElementById('link1').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link2').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link3').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link4').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link5').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link6').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link7').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link8').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link9').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link10').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link11').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
+document.getElementById('link12').innerHTML = QUOTES[Math.floor(Math.random() * QUOTES.length)];
